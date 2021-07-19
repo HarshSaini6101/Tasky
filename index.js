2b1311207559ab7c07cc94118b599b9800c7c0cc
harsh saini <sainiharsh6101@gmail.com>
2021-07-19 21:06:53 +0530

    add save data to local storage

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const newcard = ({
           <a href="#" class="btn btn-primary">${tasktype}</a>
         </div>
     <div class="card-footer text-muted ">
-        <button type="button" class="btn btn-outline-primary float-end rounded-pill">Open Task</button>
+        <button type="button" id=${id} class="btn btn-outline-primary float-end rounded-pill">Open Task</button>
     </div>
   </div> 
 </div>`;
@@ -101,5 +101,43 @@ const editCard = (event) => {
     taskTitle.setAttribute("contenteditable", "true");
     taskDescription.setAttribute("contenteditable", "true");
     taskType.setAttribute("contenteditable", "true");
+    submitButton.setAttribute("onclick", "saveEditchanges.apply(this, arguments)");
     submitButton.innerHTML = "Save Changes" ;
+};
+
+const saveEditchanges = (event) => {
+    event = window.event;
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+
+    let parentElement;
+    if(tagname === "Button"){
+        parentElement =  event.target.parentNode.parentNode;
+    }
+    else{
+       parentElement = event.target.parentNode.parentNode.parentNode
+    }
+    let taskTitle = parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+
+    const updateData = {
+        taskTitle: taskTitle.innerHTML,
+        taskType: tasktype.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+    };
+    globalStore = globalStore.map((task) => {
+        if (task.id === targetID) {
+            return {
+                id: task.id,
+                imageurl: task.imageurl,
+                taskTitle: updateData.taskTitle,
+                taskType: updateData.taskType,
+                taskDescription: updateData.taskDescription,
+            };
+        }
+        return;
+    });
+    updateLocalStorage(); 
 };
